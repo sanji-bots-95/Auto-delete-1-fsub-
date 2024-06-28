017271054c6e1f9bb28ab1e36258950a7caea0bc
sanji-bots-95 <aizbhig659@smail.pics>
2024-06-28 14:42:28 +0530

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/sanji-bots-95/Auto-delete-1-fsub- /Auto-delete-1-fsub-
+cd /Auto-delete-1-fsub-
+pip3 install -r requirements.txt
+echo "Starting Bot...."
+python3 main.py
